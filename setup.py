8af7aa3c329e42e47dce81c4feab0aedcbaaa32d
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-05-07 17:38:24 +0200

    Bump version: 0.0.10 → 0.0.11

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.10',
+    version='0.0.11',
     url='https://github.com/mjirik/gco_python',
     author='',
     author_email='',
