a6c4db7337c6fffa5436e574933af6ee7895ee6d
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-05-07 17:32:59 +0200

    pywget dependency removed

--- setup.py
+++ setup.py
@@ -5,8 +5,15 @@ import Cython.Distutils
 from Cython.Distutils import build_ext
 import os
 import numpy
-import wget
+import sys, os, tempfile, logging
+# import wget
 import zipfile
+if sys.version_info >= (3,):
+    import urllib.request as urllib2
+    import urllib.parse as urlparse
+else:
+    import urllib2
+    import urlparse
 
 gco_directory = "gco_src"
 
@@ -16,12 +23,57 @@ files = ['GCoptimization.cpp', 'graph.cpp', 'LinkedBlockList.cpp',
 files = [os.path.join(gco_directory, f) for f in files]
 files.insert(0, "gco_python.pyx")
 
+def download_file(url, dest=None, filename=None):
+    """
+    Download and save a file specified by url to dest directory,
+
+    Inspired by:
+    PabloG, Stan and Steve Barnes
+    https://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+    """
+    u = urllib2.urlopen(url)
+
+    scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
+    if filename is None:
+        filename = os.path.basename(path)
+    if not filename:
+        filename = 'downloaded.file'
+    if dest:
+        filename = os.path.join(dest, filename)
+
+    with open(filename, 'wb') as f:
+        meta = u.info()
+        meta_func = meta.getheaders if hasattr(meta, 'getheaders') else meta.get_all
+        meta_length = meta_func("Content-Length")
+        file_size = None
+        if meta_length:
+            file_size = int(meta_length[0])
+        print("Downloading: {0} Bytes: {1}".format(url, file_size))
+
+        file_size_dl = 0
+        block_sz = 8192
+        while True:
+            buffer = u.read(block_sz)
+            if not buffer:
+                break
+
+            file_size_dl += len(buffer)
+            f.write(buffer)
+
+            status = "{0:16}".format(file_size_dl)
+            if file_size:
+                status += "   [{0:6.2f}%]".format(file_size_dl * 100 / file_size)
+            status += chr(13)
+            print(status, end="")
+
+
 # download src files
 # wget.download("http://vision.csd.uwo.ca/code/gco-v3.0.zip")
 gco_zip = "gco-v3.0.zip"
 if not os.path.exists(gco_zip):
     print("Downloading gco-v3.0.zip ...")
-    wget.download("http://home.zcu.cz/~mjirik/lisa/install/gco-v3.0.zip", out=gco_zip)
+    download_file("http://home.zcu.cz/~mjirik/lisa/install/gco-v3.0.zip", filename=gco_zip)
+    # wget.download("http://home.zcu.cz/~mjirik/lisa/install/gco-v3.0.zip", out=gco_zip)
 if not os.path.exists(gco_directory):
     os.makedirs(gco_directory)
 if not os.path.exists(os.path.join(gco_directory, 'graph.h')):
@@ -68,5 +120,5 @@ setup(
                             library_dirs=[gco_directory],
                             extra_compile_args=["-fpermissive"])],
     package_data = {'': ['pygco.so']},
-    install_requires=['cython', 'wget'],
+    install_requires=['cython'],
 )
